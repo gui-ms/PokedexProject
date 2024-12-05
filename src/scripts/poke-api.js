4777702243cfc180116b904575d0126b923a845f
Guilherme Martins <gui.martins.soares@gmail.com>
2024-12-05 19:09:13 -0300

    Updated pokemon card customization

--- src/scripts/poke-api.js
+++ src/scripts/poke-api.js
@@ -1,12 +1,28 @@
 const pokeApi = {};
 
+function convertPokeApiDetailToPokemonClass(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.name = pokeDetail.name;
+    pokemon.number = pokeDetail.id;
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    pokemon.types = types;
+    const [mainType] = pokemon.types;
+    pokemon.type = mainType;
+    pokemon.photo = pokeDetail.sprites.other["official-artwork"].front_default;
+    pokemon.cry = pokeDetail.cries.latest;
+    return pokemon;
+    
+}
+
 pokeApi.getPokemonDetails = (pokemon) => {
     return fetch(pokemon.url)
     .then((response) => response.json())
+    .then((convertPokeApiDetailToPokemonClass))
     .catch((error) => console.error(error))
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
     .then((response) => response.json())
